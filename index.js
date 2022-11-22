14260bf0b2526544de116a5948016117dcd73744
Blzher <95661896+Blzher@users.noreply.github.com>
2022-11-21 22:31:52 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var password = "unblocker";
+function passcheck() {
+    if(document.getElementById('pass1').value != password) {
+alert('Wrong Password, Try Again.');
+return false;
+    }
+    if(document.getElementById(pass1).value == password) {
+        alert('Correct Password! Click OK to enter webpage.');
+    }
+    }
